3341591552e39aabbe66cc09971f84bae1887efb
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-01-03 16:26:57 -0300

    define before and after

--- spec/checkboxes_spec.rb
+++ spec/checkboxes_spec.rb
@@ -1,5 +1,14 @@
-describe 'Caixas de seleção' do
+describe 'Caixas de seleção', :checkbox do
+    before(:each) do
+    visit 'https://training-wheels-protocol.herokuapp.com/checkboxes'
+    end
     it 'Marcando uma opção' do
-        
+        check('thor')
+    end
+    it 'Desmarcando uma opção' do
+        uncheck('antman')
+    end
+    after(:each) do
+        sleep 3
     end
 end
--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -6,7 +6,7 @@ describe 'Forms' do
 
     click_button 'Login'
     expect(find('#flash').visible?).to be true
-
-    expect(find('#flash').to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+    expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
   end
 end
